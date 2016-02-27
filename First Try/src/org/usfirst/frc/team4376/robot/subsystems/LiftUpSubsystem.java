901072fb35250ebf98f615a4ad160b6cc7b9d95a
Burke Ramsey <birk5437@gmail.com>
2016-02-27 18:52:27 -0500

    hopefully this works!...?

--- First Try/src/org/usfirst/frc/team4376/robot/subsystems/LiftUpSubsystem.java
+++ First Try/src/org/usfirst/frc/team4376/robot/subsystems/LiftUpSubsystem.java
@@ -4,6 +4,7 @@ import org.usfirst.frc.team4376.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 
@@ -16,17 +17,21 @@ public class LiftUpSubsystem extends Subsystem {
     // here. Call these from Commands.
 	
 	CANTalon liftMotor = new CANTalon(RobotMap.LIFT_MOTOR);
+	Encoder liftAngleEncoder;
 	//DoubleSolenoid shooterBrake;
 	
 	public LiftUpSubsystem() {
 		liftMotor = new CANTalon(RobotMap.LIFT_MOTOR);
+		liftAngleEncoder = new Encoder(RobotMap.LIFT_ANGLE_ENCODER_A, RobotMap.LIFT_ANGLE_ENCODER_B, true, Encoder.EncodingType.k1X);
 		//shooterBrake = new DoubleSolenoid(RobotMap.LOCK_SHOOTER_FORWARD_SOLENOID, RobotMap.LOCK_SHOOTER_REVERSE_SOLENOID);
 		
 	}
 	
 	public void robotUp(){
-		liftMotor.set(0.375);
-		System.out.println("robotUp is called");
+		int currentEncoderValue = liftAngleEncoder.get();
+		while (liftAngleEncoder.get() < currentEncoderValue + 5) { //TODO: I just picked 5 randomly...change accordingly
+		  liftMotor.set(0.7);  //original value was 0.375
+		}
 		//shooterBrake.set(DoubleSolenoid.Value.kForward);
 	}
 	
@@ -36,11 +41,14 @@ public class LiftUpSubsystem extends Subsystem {
 	}
 	
 	public void robotDown(){
-		liftMotor.set(-0.375);
+		int currentEncoderValue = liftAngleEncoder.get();
+		while (liftAngleEncoder.get() > currentEncoderValue - 5) { //TODO: I just picked 5 randomly...change accordingly
+		  liftMotor.set(-0.7);  //original value was -0.375
+		}
 		//shooterBrake.set(DoubleSolenoid.Value.kForward);
 		//shooterBrake.set(DoubleSolenoid.Value.kOff);
 	}
-
+	
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
