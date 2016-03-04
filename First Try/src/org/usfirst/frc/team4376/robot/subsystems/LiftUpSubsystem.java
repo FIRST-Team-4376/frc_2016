85c0d582b2e9d065d09dcf40e04a659aad61607f
Grace Ochs <brightonrock39@gmail.com>
2016-03-04 11:59:20 -0500

    auton will hopefully work well and not crash the robot

--- First Try/src/org/usfirst/frc/team4376/robot/subsystems/LiftUpSubsystem.java
+++ First Try/src/org/usfirst/frc/team4376/robot/subsystems/LiftUpSubsystem.java
@@ -49,7 +49,7 @@ public class LiftUpSubsystem extends Subsystem {
 	}
 	
 	public void robotDown(){
-		liftMotor.set(-0.375);
+		liftMotor.set(-0.25);
 		//shooterBrake.set(DoubleSolenoid.Value.kForward);
 		//shooterBrake.set(DoubleSolenoid.Value.kOff);
 	}
