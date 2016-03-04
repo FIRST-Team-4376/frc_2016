bdf91e91625a2915ff2baf8ba3026598c39f0bad
Burke Ramsey <birk5437@gmail.com>
2016-03-03 20:47:17 -0500

    double camera the hard way?

--- First Try/src/org/usfirst/frc/team4376/robot/subsystems/TapeMeasureSubsystem.java
+++ First Try/src/org/usfirst/frc/team4376/robot/subsystems/TapeMeasureSubsystem.java
@@ -7,6 +7,9 @@ import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.vision.USBCamera;
 
+import com.ni.vision.NIVision.Image;
+import com.ni.vision.NIVision;
+
 
 /**
  *
@@ -21,13 +24,16 @@ public class TapeMeasureSubsystem extends Subsystem {
 	CameraServer camServer2;
 	USBCamera lifecam;
 	USBCamera lifecam2;
+	
+	int session;
+	Image frame;
 	//DoubleSolenoid shooterBrake;
 	
 	public TapeMeasureSubsystem() {
 		//shooterBrake = new DoubleSolenoid(RobotMap.LOCK_SHOOTER_FORWARD_SOLENOID, RobotMap.LOCK_SHOOTER_REVERSE_SOLENOID);
 		
-		camServer = CameraServer.getInstance();
-		lifecam = new USBCamera("cam0");
+		// camServer = CameraServer.getInstance();
+		// lifecam = new USBCamera("cam0");
 		//camServer2 = CameraServer.getInstance();
 		//lifecam2 = new USBCamera("cam1");
 		
@@ -54,26 +60,44 @@ public class TapeMeasureSubsystem extends Subsystem {
 	}
 	
 	public void switchtoCamera1(){
+
+	  // the camera name (ex "cam0") can be found through the roborio web interface
+	  session = NIVision.IMAQdxOpenCamera("cam0",
+	          NIVision.IMAQdxCameraControlMode.CameraControlModeController);
+	  NIVision.IMAQdxConfigureGrab(session);
+
+	  NIVision.IMAQdxGrab(session, frame, 1);
+	  CameraServer.getInstance().setImage(frame);
+
+
 		//lifecam.closeCamera();
 	    //lifecam2.closeCamera();
-		lifecam.closeCamera();
-		lifecam = new USBCamera("cam0");
-	    lifecam.setFPS(30);
-        lifecam.openCamera();
-        camServer.startAutomaticCapture(lifecam);
+		// lifecam.closeCamera();
+		// lifecam = new USBCamera("cam0");
+	 //    lifecam.setFPS(30);
+  //       lifecam.openCamera();
+  //       camServer.startAutomaticCapture(lifecam);
     }
 	
 	public void switchtoCamera2(){
+	  session = NIVision.IMAQdxOpenCamera("cam1",
+	          NIVision.IMAQdxCameraControlMode.CameraControlModeController);
+	  NIVision.IMAQdxConfigureGrab(session);
+
+	  NIVision.IMAQdxGrab(session, frame, 1);
+	  CameraServer.getInstance().setImage(frame);
+
+
 		//lifecam.closeCamera();
 		//lifecam2.closeCamera();
 		//lifecam2.setFPS(30);
         //lifecam2.openCamera();
-	    lifecam.closeCamera();
-		lifecam.closeCamera();
-		lifecam = new USBCamera("cam1");
-	    lifecam.setFPS(30);
-        lifecam.openCamera();
-        camServer2.startAutomaticCapture(lifecam);
+	 //    lifecam.closeCamera();
+		// lifecam.closeCamera();
+		// lifecam = new USBCamera("cam1");
+	 //    lifecam.setFPS(30);
+  //       lifecam.openCamera();
+  //       camServer2.startAutomaticCapture(lifecam);
 	}
 
     public void initDefaultCommand() {
