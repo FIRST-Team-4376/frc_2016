03dabe17f50ac9997492e2a1624220459cc466cb
Burke Ramsey <birk5437@gmail.com>
2017-02-20 18:40:35 -0500

    drive code is in place!

--- Maybe2017/src/org/usfirst/frc/team4376/robot/subsystems/VisionSubsystem.java
+++ Maybe2017/src/org/usfirst/frc/team4376/robot/subsystems/VisionSubsystem.java
@@ -23,11 +23,23 @@ public class VisionSubsystem extends Subsystem {
 	
 	public void lineUpGear(){
     	System.out.println("test");
-    	double overallCenterX = SmartDashboard.getDouble("overallCenterX", -1);
+    	double overallCenterX = SmartDashboard.getDouble("overallCenterX", -1.0);
+    	double imageWidth = SmartDashboard.getDouble("imageWidth", -1.0);
     	System.out.println("Overall Center X:" + SmartDashboard.getDouble("overallCenterX", -1));
     	System.out.println("Overall Center Y:" + SmartDashboard.getDouble("overallCenterY", -1));
     	System.out.println("Image Width:" + SmartDashboard.getDouble("imageWidth", -1));
     	System.out.println("Image Height:" + SmartDashboard.getDouble("imageHeight", -1));
+    	
+    	if (overallCenterX != -1.0 && imageWidth != 1.0 ){
+    		double imageCenter = imageWidth / 2.0;
+    		
+    		if (overallCenterX < imageCenter){
+    			Robot.chassis.driveMe(-0.5, 0.0, 0.0);
+    		} else if (overallCenterX > imageCenter){
+    			Robot.chassis.driveMe(0.5, 0.0, 0.0);
+    		}
+    	}
+    	
     	// Robot.chassis.driveForward();
 	}
 	
