e710fa97306e849d44ee851ced0003b3659161b8
Team 4376 <thisemaildoesntwork@team4376.com>
2017-03-17 19:05:29 -0400

    fix left auton

--- Maybe2017/src/org/usfirst/frc/team4376/robot/commands/LeftSideAuton.java
+++ Maybe2017/src/org/usfirst/frc/team4376/robot/commands/LeftSideAuton.java
@@ -37,7 +37,7 @@ public class LeftSideAuton extends Command {
 		// Robot.chassis.driveForward();
 		// }
 
-		if (timer.get() > 0 && timer.get() <= 3.25) {
+		if (timer.get() > 0 && timer.get() <= 3.0) {
 			if (Robot.gyro.getAngleZ() <= 2 && Robot.gyro.getAngleZ() >= -2) {
 				Robot.chassis.driveMe(0, -.25, 0);
 				// } else if (Robot.gyro.getAngleZ() <2){
@@ -58,14 +58,22 @@ public class LeftSideAuton extends Command {
 			} else if (Robot.gyro.getAngleZ() < -62) {
 				Robot.chassis.driveMe(0, -.25, .25);
 			}
-		}    else if(timer.get() > 3.25 && timer.get() <= 6.5){
+		}    else if(timer.get() > 3.0 && timer.get() <= 5.5){
 			if(Robot.gyro.getAngleZ() < 59){
 				Robot.chassis.driveMe(0, 0, .20);
 			}
 			else if (Robot.gyro.getAngleZ() > 61){
 				Robot.chassis.driveMe(0, 0, -.20);
 			}
-		}	else if(timer.get() > 6.5 && timer.get() <= 7.5){
+		}	else if (timer.get() > 5.5 && timer.get() < 6.5){
+			if(Robot.gyro.getAngleZ() < 59){
+				Robot.chassis.driveMe(0, 0, .15);
+			}
+			else if (Robot.gyro.getAngleZ() > 61){
+				Robot.chassis.driveMe(0, 0, -.15);
+			}
+		}
+		else if(timer.get() > 6.5 && timer.get() <= 7.5){
 				if(Robot.gyro.getAngleZ() < 62 && Robot.gyro.getAngleZ() > 58){
 						Robot.chassis.driveMe(0, -.25, 0);
 				} else if(Robot.gyro.getAngleZ() < 58){
