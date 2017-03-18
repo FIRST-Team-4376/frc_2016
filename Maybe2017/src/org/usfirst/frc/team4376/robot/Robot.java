f5295708e50c6d817386a4bedefaae13fceba1ae
Burke Ramsey <birk5437@gmail.com>
2017-03-18 08:47:01 -0400

    NOW we are ready to test

--- Maybe2017/src/org/usfirst/frc/team4376/robot/Robot.java
+++ Maybe2017/src/org/usfirst/frc/team4376/robot/Robot.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import org.opencv.core.Mat;
+import org.usfirst.frc.team4376.robot.commands.CenterVisionAuton;
 import org.usfirst.frc.team4376.robot.commands.CrossWhiteLineAuton;
 import org.usfirst.frc.team4376.robot.commands.ExampleCommand;
 import org.usfirst.frc.team4376.robot.commands.FirstAuton;
@@ -68,6 +69,7 @@ public class Robot extends IterativeRobot {
 		chooser.addObject("Cross White Line", new CrossWhiteLineAuton());
 		chooser.addObject("Left starting spot", new LeftSideAuton());
 		chooser.addObject("Right starting spot", new RightSideAuton());
+		chooser.addObject("Center starting spot WITH VISION", new CenterVisionAuton());
 		
 		
 		// chooser.addObject("My Auto", new MyAutoCommand());
---
+++ Maybe2017/src/org/usfirst/frc/team4376/robot/commands/CenterVisionAuton.java
@@ -0,0 +1,55 @@
+package org.usfirst.frc.team4376.robot.commands;
+
+import org.usfirst.frc.team4376.robot.Robot;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class CenterVisionAuton extends Command {
+
+	private Timer timer;
+	
+	public int iteration = 1;
+	
+    public CenterVisionAuton() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.vision);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	timer = new Timer();
+    	
+    	Robot.gyro.reset();
+    	timer.reset();
+    	timer.start();
+    	
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	if (timer.get() > 0 && timer.get() < 12.0){
+    		Robot.vision.checkForCameraUpdate();
+    	}
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	
+    	Robot.chassis.stopMe();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
